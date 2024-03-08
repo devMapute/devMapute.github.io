fda6155e1a3bbd40c2e85bda6313374f540a65cd
devMapute <ammapute1@up.edu.ph>
2024-03-08 12:21:58 +0800

    feat: added a feature to validate user info

--- exer4/index.js
+++ exer4/index.js
@@ -17,16 +17,33 @@ function generateUniqueID(firstName, lastName){
   return temp;
 }
 
+function isCorrect(array){
+  if (array.length != 0 ){ // are fields present?
+    for (var i = 0; i <= 2; i++){
+      if (array[i].length === 0){
+        return false; //empty string
+      }
+    }
+    if (Number.isInteger(array[3])){
+      if (array[3] >= 18){
+        return true; // all fields are valid
+      } else return false; //age is not allowed
+    } else return false; // last index is not a number
+  } else return false; //not complete
+}
+
 function isValidEmail(email){
   return validator.isEmail(email);
 }
 
 
 function addAccount(array){
-  if (isValidEmail(array[2])){
+  if (isCorrect(array) && isValidEmail(array[2])){
+    const userinfo = array.join(",");
     
-  }
-  return 0; 
+
+    return true;
+  }else return false;
 
 }
 
