c32f50f254a876828c2af404fb0c55cc184f6fbf
devMapute <ammapute1@up.edu.ph>
2024-03-08 12:51:49 +0800

    feat: added a function to add user info to a txt file

--- exer4/index.js
+++ exer4/index.js
@@ -1,3 +1,6 @@
+// This js adds an account to file named users.txt.
+
+
 import * as fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 import validator from 'validator';
@@ -18,32 +21,54 @@ function generateUniqueID(firstName, lastName){
 }
 
 function isCorrect(array){
-  if (array.length != 0 ){ // are fields present?
+  if (array.length == 4 ){ // are fields present?
     for (var i = 0; i <= 2; i++){
       if (array[i].length === 0){
+        console.log("There is an empty field.");
         return false; //empty string
       }
     }
     if (Number.isInteger(array[3])){
       if (array[3] >= 18){
         return true; // all fields are valid
-      } else return false; //age is not allowed
-    } else return false; // last index is not a number
-  } else return false; //not complete
+      } else {
+        console.log("Age must be 18 above");
+        return false;
+      } //age is not allowed
+    } else {
+      console.log("Enter a valid age.");
+      return false;
+    } // last index is not a number
+  } else {
+    console.log("Complete all fields!");
+    return false;
+  } //not complete
 }
 
 function isValidEmail(email){
-  return validator.isEmail(email);
+  if (validator.isEmail(email)){// true if email is valid format
+    return validator.isEmail(email);
+  } else {
+    console.log("Invalid email");
+    return validator.isEmail(email);
+  }
 }
 
 
 function addAccount(array){
-  if (isCorrect(array) && isValidEmail(array[2])){
-    const userinfo = array.join(",");
-    
-
-    return true;
-  }else return false;
+  if (isCorrect(array) && isValidEmail(array[2])){  // validate info
+    const uniqueID = generateUniqueID(array[0], array[1]); // generate code
+    array.push(uniqueID);
+    var userInfo = array.join(","); // concat
+    const filePath = 'users.txt';
+    fs.appendFile(filePath, userInfo + "\n", 'utf8', // write on file users.txt
+    // Callback function
+    function(err) { 
+        if (err) throw err;
+        //  If no error
+        console.log("Successfully added account.")
+    });
+  }
 
 }
 
--- exer4/test.js
+++ exer4/test.js
@@ -1,7 +1,16 @@
 import {generateUniqueID, addAccount} from './index.js'
 
-console.log(generateUniqueID("Alan", "Turing"))
-console.log(addAccount(["Alan", "Turing", "aturing@w3c.com", 58]))
+addAccount(["Alan", "Turing", "aturing@w3c.com", 58])
+addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43])
+addAccount(["Andre", "White", "bogart@gmail.com", 16])
+addAccount(["Andre", "White", "bogart@gmail", 18])
+addAccount(["Andre", "", "bogart@gmail", 18])
+addAccount(["Andre", "White", "bogart@gmail.com", "a"])
+addAccount(["Andre", "White", "bogart@gmail.com"])
+addAccount(["Andre", "White", "bogart@gmail.com", 18])
+
+
+
 
 
 
