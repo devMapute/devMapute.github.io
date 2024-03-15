92dbd992fe2f1319bee63c0d0e98b0928a678e01
devMapute <ammapute1@up.edu.ph>
2024-03-15 16:15:06 +0800

    feat: added /find-by-author and /find-by-isbn-author routes

--- exer5/exer/request.js
+++ exer5/exer/request.js
@@ -18,3 +18,70 @@ needle.post(
   }
 );
 
+needle.post(
+  'http://localhost:3000/add-book',
+  {
+    bookName: 'Harry Potter and the Chamber of Secrets',
+    isbn: '0-7475-3849-2',
+    author: 'J.K Rowling',
+    yearPublished: '1998'
+  },
+  (err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(res.body);
+    }
+  }
+);
+
+needle.post(
+  'http://localhost:3000/add-book',
+  {
+    bookName: 'The Little Prince',
+    isbn: '978-0156012195',
+    author: 'Antoine Saint-Exupery',
+    yearPublished: '1943'
+  },
+  (err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(res.body);
+    }
+  }
+);
+
+
+needle.post(
+  'http://localhost:3000/add-book',
+  {
+    bookName: '',                       // success: false
+    isbn: '978-0156012195',
+    author: 'Antoine Saint-Exupery',
+    yearPublished: '1943'
+  },
+  (err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(res.body);
+    }
+  }
+);
+
+needle.get('http://localhost:3000/find-by-author?author=J.K+Rowling', (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(body);
+  }
+});
+
+needle.get('http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9&author=J.K+Rowling', (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(body);
+  }
+});
--- exer5/exer/server.js
+++ exer5/exer/server.js
@@ -8,31 +8,86 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-
-
 app.post('/add-book', (req, res) => {
-
-  if(req.body.bookName == '' || req.body.isbn == '' || req.body.author == '' || req.body.yearPublished == ''){
+  if (
+    req.body.bookName === '' ||
+    req.body.isbn === '' ||
+    req.body.author === '' ||
+    req.body.yearPublished === ''
+  ) {
     res.json({ success: false });
     return;
   }
 
-  let bookData = req.body.bookName+','+req.body.isbn+','+req.body.author+','+req.body.yearPublished+'\n';
+  const bookData = `${req.body.bookName},${req.body.isbn},${req.body.author},${req.body.yearPublished}\n`;
 
-  fs.appendFile('books.txt', bookData, (err) =>{
-    if (err){
+  fs.appendFile('books.txt', bookData, (err) => {
+    if (err) {
+      console.error(err);
       res.json({ success: false });
-    }else {
+    } else {
       res.json({ success: true });
-  }
-  } );
+    }
+  });
 });
 
+
 app.get('/find-by-isbn-author', (req, res) => {
-  console.log(req);
-  console.log(res);
-  res.send('Hello! This is Andrae\'s server.');
-  
+  const { isbn, author } = req.query;
+
+  fs.readFile('books.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.json({ success: false });
+    } else {
+      const lines = data.split('\n');
+      const matchingBooks = lines.filter((line) => {
+        const [bookName, bookIsbn, bookAuthor, bookYearPublished] = line.split(',');
+        return bookIsbn === isbn && bookAuthor === author;
+      });
+
+      const formattedBooks = matchingBooks.map((book) => {
+        const [bookName, bookIsbn, bookAuthor, bookYearPublished] = book.split(',');
+        return {
+          Title: bookName,
+          Isbn: bookIsbn,
+          Author: bookAuthor,
+          YearPublished: bookYearPublished
+        };
+      });
+
+      res.json({ books: formattedBooks });
+    }
+  });
+});
+
+app.get('/find-by-author', (req, res) => {
+  const { author } = req.query;
+
+  fs.readFile('books.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.json({ success: false });
+    } else {
+      const lines = data.split('\n');
+      const matchingBooks = lines.filter((line) => {
+        const [name, bookISBN, bookAuthor] = line.split(',');
+        return bookAuthor === author;
+      });
+
+      const formattedBooks = matchingBooks.map((book) => {
+        const [bookName, bookIsbn, bookAuthor, bookYearPublished] = book.split(',');
+        return {
+          Title: bookName,
+          Isbn: bookIsbn,
+          Author: bookAuthor,
+          YearPublished: bookYearPublished
+        };
+      });
+
+      res.json({ books: formattedBooks });
+    }
+  });
 });
 
 app.listen(3000, () => { console.log('Server started at port 3000')} );
